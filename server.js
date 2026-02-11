cc15d79cd950591e54713dff2f13a20a051f17c1
ABDUL HAKEEM AMER <120081228+Hakeem0311@users.noreply.github.com>
2026-02-11 15:24:03 +0530

    Fix backend for Render

--- server.js
+++ server.js
@@ -219,6 +219,8 @@ app.post("/api/change-password", auth, async (req, res) => {
 
 
 // Start server
-app.listen(4000, () => {
-  console.log("Server running on http://localhost:4000");
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
+
